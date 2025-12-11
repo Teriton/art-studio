cfeb975c2c4cac7dfb4ef990f93cfb2556495760
Teriton <subochartem2007@mail.ru>
2025-12-11 14:41:38 +0300

    Fixed units in schedule and change design

--- frontend/src/lib/components/Schedule/ScheduleCard.svelte
+++ frontend/src/lib/components/Schedule/ScheduleCard.svelte
@@ -13,7 +13,7 @@
 <div class="gap-4 rounded-3xl bg-gray-400/40 p-4 shadow-xl">
     <h2 class=" font-semibold text-2xl ml-4 md:text-md">{workshop.title}</h2>
     <hr/>
-    <div class="flex flex-col md:flex-row md:justify-between md:items-center p-4 md:p-0">
+    <div class="flex flex-col md:flex-row md:justify-between md:items-center mt-4 md:p-0">
         <div class="grid grid-flow-col mb-6 md:mb-0 grid-rows-1 md:grid-rows-2">
             <div>
                 <p class="text-gray-700 font-light text-xl md:text-base">Мастер</p>
@@ -25,10 +25,22 @@
             </div>
         </div>
         <div class=" grid grid-cols-2 gap-4 md:mx-10 md:content-around md:h-full">
-            <p class= "flex items-center justify-center bg-gray-600/40 py-3 p-2 md:p-2 shadow-xl rounded-3xl">Статус: {workshop.status}</p>
-            <p class="flex items-center justify-center bg-gray-600/40 py-3 p-2 md:p-2  shadow-xl rounded-3xl">Сумма: {workshop.fee} </p>
-            <p class="flex items-center justify-center bg-gray-600/40 py-3 p-2 md:p-2  shadow-xl rounded-3xl">Сложность: {workshop.dificulty}</p>
-            <p class="flex items-center justify-center bg-gray-600/40 py-3 p-2 md:p-2  shadow-xl rounded-3xl">Продолжительность: {workshop.duration}</p>
+            <div class="flex flex-col justify-centerpx-10 py-3 p-2 md:px-10 md:p-2">
+                <p class="text-gray-700 font-light text-xl md:text-base">Статус:</p>
+                <p class="font-mediums text-2xl md:text-base"> {workshop.status}</p>
+            </div>
+            <div class="flex flex-col justify-centerpx-10 py-3 p-2 md:px-10 md:p-2">
+                <p class="text-gray-700 font-light text-xl md:text-base">Сумма:</p>
+                <p class="font-mediums text-2xl md:text-base"> {workshop.fee} руб.</p>
+            </div>
+            <div class="flex flex-col justify-centerpx-10 py-3 p-2 md:px-10 md:p-2">
+                <p class="text-gray-700 font-light text-xl md:text-base">Сложность: </p>
+                <p class="font-mediums text-2xl md:text-base"> {workshop.dificulty}</p>
+            </div>
+            <div class="flex flex-col justify-centerpx-10 py-3 p-2 md:px-10 md:p-2">
+                <p class="text-gray-700 font-light text-xl md:text-base">Продолжительность:</p>
+                <p class="font-mediums text-2xl md:text-base">{workshop.duration} мин.</p>
+            </div>
         </div>
         <div class="flex gap-3 justify-centersss text-2xl md:text-xl font-light items-center mt-6 md:flex-col">
             <button class=" bg-red-600/30 h-15 md:w-full shadow-xl flex-1" onclick={select}>Детали</button>
